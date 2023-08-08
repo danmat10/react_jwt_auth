af3c308b4689a3a70cfbf2492c065e27de2d0c7d
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-08 16:40:13 -0300

    refactor: index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>

11819f621fc2ff058ea2e286eeff3becc298f01c
Daniel Matthes <76593666+danmat10@users.noreply.github.com>
2023-08-08 19:47:20 -0300

    refactor: Header.component

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withSignOut } from "react-auth-kit";
+import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -41,10 +42,17 @@ class Header extends Component {
         <AppBar position="static" elevation={2}>
           <Toolbar>
             <MenuButton onClick={this.toggleSidebar} />
+
             <Typography
               variant="h5"
-              component="div"
-              sx={{ flexGrow: 1, fontWeight: "bold" }}
+              component={Link} // Define o componente Typography como um Link
+              to="/" // Caminho para o qual o Link deve redirecionar
+              sx={{
+                flexGrow: 1,
+                fontWeight: "bold",
+                textDecoration: "none", // Remover sublinhado típico de links
+                color: "inherit", // Faz com que a cor do texto seja a mesma, independente de ser um link
+              }}
             >
               Nome da Aplicação
             </Typography>
